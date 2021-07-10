ec1bac2c3b5aaf844ff695bb4e190222beacbf93
kcsarith <krisnasarith@gmail.com>
2021-07-10 09:36:15 -0700

    Create index.vue

---
+++ client/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <div class="container">
+      <Hero/>
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+        />
+        <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+        />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+import { largeCardSections, smallCardSections } from "../assets/data.js"
+export default Vue.extend({
+  data(){
+    return {largeCardInfo: largeCardSections, smallCardSections}
+    }
+  }
+)
+</script>
