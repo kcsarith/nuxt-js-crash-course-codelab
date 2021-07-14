cc3d3d6b085e01ce3823c9a99998b6ffdec1c611
kcsarith <krisnasarith@gmail.com>
2021-07-14 00:14:25 -0700

    Create index.vue

---
+++ client/pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <div class="container">
+        <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo.slice(0,1)"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+        />
+        <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+        />
+</div>
+</div>
+</template>
+
+<script>
+    import {largeCardSections, smallCardSections} from "../../assets/data.js"
+    export default {
+        data(){
+            return  {largeCardInfo: largeCardSections,
+            smallCardSections: smallCardSections
+        }
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
