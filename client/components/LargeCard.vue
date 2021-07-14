395540df1cbc512e6c8609696145e93a481dfdb1
kcsarith <krisnasarith@gmail.com>
2021-07-13 19:54:30 -0700

    Create LargeCard.vue

---
+++ client/components/LargeCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <NuxtLink :to="`products/${card.id}`" class="card">
+        <img class="image" :src="require(`../static/images/${card.image || 'fe1.jpg'}`)" alt="">
+        <h3 class="header">{{ card.title }}</h3>
+        <p class="snippet">
+            {{card.snippet }}
+        </p>
+    </NuxtLink>
+</template>
+
+<script>
+    export default {
+        props: ['card'],
+    }
+</script>
+
+<style scoped>
+    .card {
+        width: 31.5%;
+        height: 25rem;
+        border: none;
+        overflow: hidden;
+        padding: 0;
+        cursor: pointer;
+    }
+    .image {
+        height: 65%;
+        border-radius: 0.5rem;
+    }
+    .header {
+        font-size: 1.15rem;
+        margin-top: 0.4rem;
+        text-decoration: none;
+    }
+    .header:hover{
+        color:black;
+    }
+    .snippet {
+        color: grey
+    }
+</style>
